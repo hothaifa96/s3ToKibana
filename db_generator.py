48ebe78fbc2355a8a7877d5136e6dfcc0e8ac737
hothaifa zoubi <hothaifazoubi@gmail.com>
2024-01-16 11:43:24 +0200

    ss

--- db_generator.py
+++ db_generator.py
@@ -12,10 +12,12 @@ db_password = "Pa$$w0rd"
 sql_file_path = "./all.sql"
 
 
-def execute_sql_script(connection, cursor, sql_file_path):
-    with open(sql_file_path, 'r') as sql_file:
-        sql_script = sql_file.read()
-
+def execute_sql_script(connection, cursor, sql_file_path,query=None):
+    if sql_file_path is not None:
+        with open(sql_file_path, 'r') as sql_file:
+            sql_script = sql_file.read()
+    else:
+        sql_script= query
     # Execute the SQL script
     cursor.execute(sql_script)
     connection.commit()
@@ -37,7 +39,39 @@ try:
 
     # Execute SQL script
     execute_sql_script(connection, cursor, sql_file_path)
-
+    clean_up= """    WITH DuplicateCTE AS (
+    SELECT
+        answer_option_id,
+        ROW_NUMBER() OVER (PARTITION BY question_id, answer_text ORDER BY answer_option_id) AS row_num
+    FROM
+        answer_options
+    WHERE
+        (question_id, answer_text) IN (
+            SELECT
+                question_id,
+                answer_text
+            FROM
+                answer_options
+            GROUP BY
+                question_id,
+                answer_text
+            HAVING
+                COUNT(*) > 1
+        )
+)
+DELETE FROM
+    answer_options
+WHERE
+    answer_option_id IN (
+        SELECT
+            answer_option_id
+        FROM
+            DuplicateCTE
+        WHERE
+            row_num > 1
+    );
+"""
+    execute_sql_script(connection,cursor,None,query=clean_up)
     print("SQL script executed successfully.")
 
 except psycopg2.Error as e:
